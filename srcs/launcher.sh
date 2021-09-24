6f736b7b5dee72bd0c98bfefc80880c3dc017e54
Arthur Tinseau <arthurtinseau@MacBook-Pro-de-Arthur.local>
2021-09-24 14:35:49 +0200

    fix mac

--- srcs/launcher.sh
+++ srcs/launcher.sh
@@ -6,7 +6,6 @@ if [ "$SUDO_USER" = "" ]; then
 else
 	U=$SUDO_USER
 fi;
-H="/home/$U"
 URL=${U}.42.fr
 
 remove_line ()
@@ -17,12 +16,15 @@ remove_line ()
 }
 
 if [ "$1" = "init" ]; then
-	mkdir ${H}/data &> /dev/null
-	mkdir ${H}/data/wordpress &> /dev/null
-	mkdir ${H}/data/db &> /dev/null
+	mkdir ${HOME}/data &> /dev/null
+	mkdir ${HOME}/data/wordpress &> /dev/null
+	mkdir ${HOME}/data/db &> /dev/null
+
+	sudo chown -R $U ${HOME}/data
+
 	sudo echo "127.0.0.1		$URL" >> /etc/hosts
 elif [ "$1" = "clear" ]; then
-	sudo rm -rf ${H}/data
+	sudo rm -rf ${HOME}/data
 	docker volume rm inception_data &> /dev/null
 	docker volume rm inception_database &> /dev/null
 	remove_line "127.0.0.1		$URL"
